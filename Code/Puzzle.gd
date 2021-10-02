43177ab12199123b8f80623680121d9ff33f7c98
Naomi <naomi.j.sprague@gmail.com>
2021-10-02 17:29:34 -0500

    Naomi branch (#15)
    * working slider phase 1
    
    * removed old text, game still works
    
    * puzzle based on slider minmax
    
    * wiggly line 2 connects to slider

--- Code/Puzzle.gd
+++ Code/Puzzle.gd
@@ -7,7 +7,11 @@ var solution
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	solution = randi() % 4 # random int between 0 and 3
+	#solution = randi() % 4 # random int between 0 and 3
+	
+
+func generate_puzzle(slider_min, slider_max):
+	solution = randi() % (int(slider_max) + 1) + int(slider_min)
 	$EngineGraph.draw_from_puzzle(solution)
 	$Solution.text = str(solution)
 
