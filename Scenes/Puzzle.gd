b99f2577d96e5fee00e66d6674d0b7ca176f3c1e
Kichiguy <sedarmor@gmail.com>
2021-10-02 12:42:13 -0500

    Added puzzle destruction after game ends

--- Scenes/Puzzle.gd
+++ Scenes/Puzzle.gd
@@ -5,7 +5,7 @@ var solution
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	var solution = randi() % 4 # random int between 0 and 3
+	solution = randi() % 4 # random int between 0 and 3
 	$Solution.text = str(solution)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
