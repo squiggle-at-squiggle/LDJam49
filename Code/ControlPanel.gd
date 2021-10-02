e59ac9d4ac6c6ab4a2cd804c573eba91bb18724e
naomijsprague <naomi.j.sprague@gmail.com>
2021-10-02 17:27:25 -0500

    wiggly line 2 connects to slider

--- Code/ControlPanel.gd
+++ Code/ControlPanel.gd
@@ -3,11 +3,13 @@ extends CanvasLayer
 signal submitted
 
 # Declare member variables here. Examples:
-var submission
+var submission = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Control/EngineGraph.draw_from_puzzle(submission)
 	pass # Replace with function body.
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -16,3 +18,7 @@ func _ready():
 func _on_Submit_pressed():
 	submission = $Control/SliderOne/HSlider.value
 	emit_signal("submitted")
+
+
+func _on_SliderOne_changed():
+	$Control/EngineGraph.draw_from_puzzle($Control/SliderOne/HSlider.value)
