d062067f3ea3e3d4e424db5f3cc12c9a86ca91cb
naomijsprague <naomi.j.sprague@gmail.com>
2021-10-02 17:17:32 -0500

    puzzle based on slider minmax

--- Code/Main.gd
+++ Code/Main.gd
@@ -21,6 +21,7 @@ func NewGame():
 	$EndScreen/Control.hide()
 	var instance = Puzzle.instance()
 	# var instance2 = puzzle.instance()
+	instance.generate_puzzle($ControlPanel/Control/SliderOne/HSlider.min_value,$ControlPanel/Control/SliderOne/HSlider.max_value)
 	add_child(instance)
 	solution1 = instance.solution
 	# add_child(instance2)
