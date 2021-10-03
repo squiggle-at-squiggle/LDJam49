d4fc9d4a99f5d8653b66fd2d56eed854fafe99f1
Kichiguy <sedarmor@gmail.com>
2021-10-03 16:51:25 -0500

    Added an alarm that sounds at 5 seconds remaining. Turns off if the number goes above it

--- Code/Countdown.gd
+++ Code/Countdown.gd
@@ -3,21 +3,24 @@ extends Control
 signal time_up
 
 # Declare member variables here. Examples:
-
-
+var active
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	active = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$TimerText.text = str(floor($Countdown.time_left))
+	if $Countdown.time_left <= 5 && active:
+		alarm_on()
+	else:
+		alarm_off()
 
 func set_timer(initial_time):
 	$Countdown.wait_time = initial_time
 	$Countdown.start()
-
+	active = true
 
 func increase_time(increase_amount):
 	$Countdown.wait_time
@@ -27,4 +30,14 @@ func stop_timer():
 	$Countdown.stop()
 
 func _on_Countdown_timeout():
+	$AudioStreamPlayer.stop()
 	emit_signal("time_up")
+
+
+func alarm_on():
+	if !$AudioStreamPlayer.playing:
+		$AudioStreamPlayer.play()
+		
+func alarm_off():
+	if $AudioStreamPlayer.playing:
+		$AudioStreamPlayer.stop()
