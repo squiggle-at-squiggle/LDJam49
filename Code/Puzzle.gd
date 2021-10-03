80cf6044f7ccc3d2a511d724b35361ea07105680
Naomi <naomi.j.sprague@gmail.com>
2021-10-03 17:43:33 -0500

    Rebalance sliders (#34)
    * fixing draw step 1
    
    * verifies win/loss on all sliders
    
    * fixing lose/win bug

--- Code/Puzzle.gd
+++ Code/Puzzle.gd
@@ -19,9 +19,10 @@ func generate_puzzle(slider_min, slider_max):
 	solution2 = randi() % (int(slider_max) + 1) + int(slider_min)
 	solution3 = randi() % (int(slider_max) + 1) + int(slider_min)
 	solution4 = randi() % (int(slider_max) + 1) + int(slider_min)
-	solution5 = randi() % (int(slider_max) + 1) + int(slider_min)
+	var truthy = [1,-1]
+	solution5 = truthy[randi() % 2]
 	$EngineGraph.draw_from_puzzle(solution,solution2,solution3,solution4,solution5)
-	$Solution.text = str(solution)
+	$Solution.text = str(solution, solution2, solution3, solution4, solution5)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
